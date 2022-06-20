25f752bbc02ead4bcabe115881e886d8d2bb58a7
carlafaes <carlafaes18@hotmail.com>
2022-06-20 15:04:14 -0300

    m

--- 03_Estructurando_evaluaciones/03_Patrones_al_inicio_y_al_final/app.js
+++ 03_Estructurando_evaluaciones/03_Patrones_al_inicio_y_al_final/app.js
@@ -1,9 +1,9 @@
-var expresionInicio = /^Any/;
-var expresionFin = /JavaScript$/
-var expIniciFin = /^Any|JavaScript$/
-
+var expresionInicio = /^Any/;//se busca el patron en el inicio de la cadena
+var expresionFin = /JavaScript$/ ///se busca el patron en el final de la cadena
+var expIniciFin = /^Any|JavaScript$/ ///se busca el patron en el inicio y final de la cadena
+//[^] el circunflejo se puede usar como negacion solo si se encuentra dentro de corchetes.
 // ^ Inicio
 // $ Fin
 
 
-console.log(expIniciFin.test('Any app that can be imagined can be made in JavaScript'));
+console.log(expIniciFin.test('Any app that can be imagined can be made in JavaScript'));//true
