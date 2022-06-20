521694d545642e81b17ac1d836c56b840cb5c8e9
carlafaes <carlafaes18@hotmail.com>
2022-06-20 15:01:04 -0300

    |

--- 03_Estructurando_evaluaciones/01_Limitando_palabras_y_cadenas_de_texto/app.js
+++ 03_Estructurando_evaluaciones/01_Limitando_palabras_y_cadenas_de_texto/app.js
@@ -1,3 +1,4 @@
-var expresion1 = /\bcat\b/;
+var expresion1 = /\bcat\b/;// \b significa que el patron es una palabra completa, no una subcadena.
+//Establece los limites desde donde a donde se evaluara el patron.
 
 console.log(expresion1.test("cat"));
--- 03_Estructurando_evaluaciones/02_Seleccion_de_patrones/app.js
+++ 03_Estructurando_evaluaciones/02_Seleccion_de_patrones/app.js
@@ -1,4 +1,7 @@
-var expresion1 = /pollo|res|pescado/;
+var expresion1 = /pollo|res|pescado/;// el operador | indica que el patron puede ser uno de los siguientes. como se uitlizaria al utilizar or ||
 console.log(expresion1);
 
-console.log(expresion1.test('El cliente pidió tacos'));
+console.log(expresion1.test('El cliente pidió tacos'));//false
+console.log(expresion1.test('El cliente pidió pollo'));//true
+console.log(expresion1.test('El cliente pidió res'));//true
+console.log(expresion1.test('El cliente pidió pescado'));//true
