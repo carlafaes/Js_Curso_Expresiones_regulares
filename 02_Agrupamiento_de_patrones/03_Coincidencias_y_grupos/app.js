57ebd9fff0047cdab9a793f91beb5c0b9dd69e92
carlafaes <carlafaes18@hotmail.com>
2022-06-20 14:54:18 -0300

    exec match

--- 02_Agrupamiento_de_patrones/03_Coincidencias_y_grupos/app.js
+++ 02_Agrupamiento_de_patrones/03_Coincidencias_y_grupos/app.js
@@ -1,5 +1,5 @@
-var expresion1 = /\d+/.exec('Del año 1984')
+var expresion1 = /\d+/.exec('Del año 1984')//.exec() es un metodo que devuelve el primer valor que encuentra, a partir de que indice se encuentra,y el input.
 
 console.log(expresion1);
 
-console.log("Hasta 1994".match(/\d+/))
+console.log("Hasta 1994".match(/\d+/))//.match() es un metodo que devuelve un array con todos los valores que encuentra, a partir de que indice se encuentra,y el input.
