adc6946d3d3e108c8a8ea2ecebc491c0b5d1bcbb
carlafaes <carlafaes18@hotmail.com>
2022-06-20 14:36:28 -0300

    repeticion patrones

--- 01_Conceptos_basicos/05_Repeticiones_y_negaciones/app.js
+++ 01_Conceptos_basicos/05_Repeticiones_y_negaciones/app.js
@@ -2,6 +2,6 @@
 
 //console.log(expresion1.test('29-junio-2007'));
 
-var expresion2 = /[^01]/;
+var expresion2 = /[^01]/;//el circunflejo sirve para negar un caracter
 
 console.log(expresion2.test('10101002100110001'));
--- 01_Conceptos_basicos/06_Patrones_y_repeticion_de_patrones/app.js
+++ 01_Conceptos_basicos/06_Patrones_y_repeticion_de_patrones/app.js
@@ -5,4 +5,5 @@ var expresion1 = /\d*/;
 console.log(expresion1.test('2008'));
 
 
-//  +   *
+//  + significa que un patron se va a repetir 1 o mas veces, no puede no repetirse al menos una vez
+//  * significa que un patron se va a repetir 0 o mas veces, puede no estar presente ninguna repeticion
