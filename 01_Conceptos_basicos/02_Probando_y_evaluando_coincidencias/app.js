646ae0df990f8719efba16034d791133e6c8d99f
carlafaes <carlafaes18@hotmail.com>
2022-06-20 14:22:41 -0300

    atajos

--- 01_Conceptos_basicos/02_Probando_y_evaluando_coincidencias/app.js
+++ 01_Conceptos_basicos/02_Probando_y_evaluando_coincidencias/app.js
@@ -1,6 +1,6 @@
-var expresion1 = new RegExp('abc');
-console.log(expresion1.test('abcdef'));
+var expresion1 = new RegExp('abc');//mecanismo que nos permite cadenas de texto en base a patrones
+console.log(expresion1.test('abcdef'));//true
 
-var expresion2 = /Hola Mundo/;
+var expresion2 = /Hola Mundo/;//mecanismo que nos permite cadenas de texto en base a patrones
 
-console.log(expresion2.test('El primer programa es Hola Mundo'));
+console.log(expresion2.test('El primer programa es Hola Mundo'));//true
